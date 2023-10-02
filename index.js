c886a170235098b308207e631f91a36a0db066d6
Thierry Vicente <119328995+StalkerVince@users.noreply.github.com>
2023-10-02 15:59:36 +0200

    first upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import chalk from 'chalk';
+
+const class1 = "Saber";
+const class2 = "Archer";
+const class3 = "Lancer";
+const class4 = "Rider";
+const class5 = "Caster";
+const class6 = "Assassin";
+const class7 = "Berserker";
+
+console.log(chalk.red(class1));
+console.log(chalk.blue(class2));
+console.log(chalk.green(class3));
+console.log(chalk.yellow(class4));
+console.log(chalk.magenta(class5));
+console.log(chalk.grey(class6));
+console.log(chalk.cyan(class7));
+
+/*Run npm start dans une console dans ce dossier pour afficher la liste*/
